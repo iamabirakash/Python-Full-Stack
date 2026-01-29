8d7d68e4aab7bfd23edc39593c2001155758ea96
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-01-29 10:05:35 +0530

    Added more question

--- OOPS/MultipleInheritance.py
+++ OOPS/MultipleInheritance.py
@@ -1,24 +1,37 @@
-class Camera:
-    def __init__(self,pixel):
-        self.pixel = pixel
-    def show(self):
-        print("MegaPixels :-",self.pixel)
+# class Camera:
+#     def __init__(self,pixel):
+#         self.pixel = pixel
+#     def show(self):
+#         print("MegaPixels :-",self.pixel)
 
-class Phone:
-    def __init__(self,name):
-        self.name = name
-    def display(self):
-        print("Phone :-",self.name)
+# class Phone:
+#     def __init__(self,name):
+#         self.name = name
+#     def display(self):
+#         print("Phone :-",self.name)
 
-class smartphone(Camera,Phone):
-    def __init__(self, pixel, name, model):
-        Camera.__init__(self,pixel)
-        Phone.__init__(self,name)
-        self.model = model
-    def info(self):
-        self.show()
-        self.display()
-        print("Model :-",self.model)
+# class smartphone(Camera,Phone):
+#     def __init__(self, pixel, name, model):
+#         Camera.__init__(self,pixel)
+#         Phone.__init__(self,name)
+#         self.model = model
+#     def info(self):
+#         self.show()
+#         self.display()
+#         print("Model :-",self.model)
     
-s = smartphone(10,"Apple",17)
-s.info()
+# s = smartphone(10,"Apple",17)
+# s.info()
+
+class Engine:
+    def move(self):
+        print("Wheels moving from Engine")
+class Wheels:
+    def wheel(self):
+        print("Wheels from Wheels")
+class Car(Engine,Wheels):
+    pass
+
+c = Car()
+c.move()
+c.wheel()
