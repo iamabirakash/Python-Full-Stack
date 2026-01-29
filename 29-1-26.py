2d8e9ed8b981a541a4443e1c26f2d5a678e28b7e
Amisha-Mehta <145385847+Amisha-Mehta@users.noreply.github.com>
2026-01-29 09:37:54 +0530

    Added example of Polymorphism

---
+++ 29-1-26.py
@@ -0,0 +1,22 @@
+#constructor overloading--> same name but diff parameters
+#overloading--> we are changing the parameters
+#overriding--> same function name, but different definition
+class vehicle:
+    def __init__(self,model,color):
+        self.model=model;
+        self.color=color;
+    def display(self):
+        print(f"Model: {self.model} and color is: {self.color}")
+class Car(vehicle):
+    #Constructor overloading
+    def __init__(self,model,color,seats):
+        super().__init__(model,color)
+        self.seats=seats
+    #method overriding
+    def display(self):
+        super().display();
+        print("seats are ", self.seats);
+obj=vehicle("BMW","Black");
+obj.display();
+obj2= Car("Creta","White",5)
+obj2.display();
