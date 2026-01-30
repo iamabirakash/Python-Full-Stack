86b0b9eef108a2d6d8039dcb87d595d9b4ce8f07
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-01-30 11:44:02 +0530

    Added more Questions

--- OOPS/MultiLevel.py
+++ OOPS/MultiLevel.py
@@ -43,25 +43,90 @@
 # print(m._empid)
 
 # Q3
-class Animal:
-    def eat(self):
-        print("Animal Eating")
-    def _eating(self):
-        print("Eatin Protectedly")
-class Dog(Animal):
-    def bark(self):
-        print("Dog is Barking")
-    def __barking(self):
-        print("Dog is Barking Privately")
-class Puppy(Dog):
-    def play(self):
-        print("Puppy is Playing")
-    def playing(self):
-        print("Puppy is Publicaly")
-p = Puppy()
-p.play()
-p.playing()
-p.bark()
-# p.barking()
-p.eat()
-p._eating()
+# class Animal:
+#     def eat(self):
+#         print("Animal Eating")
+#     def _eating(self):
+#         print("Eatin Protectedly")
+# class Dog(Animal):
+#     def bark(self):
+#         print("Dog is Barking")
+#     def __barking(self):
+#         print("Dog is Barking Privately")
+# class Puppy(Dog):
+#     def play(self):
+#         print("Puppy is Playing")
+#     def playing(self):
+#         print("Puppy is Publicaly")
+# p = Puppy()
+# p.play()
+# p.playing()
+# p.bark()
+# # p.barking()
+# p.eat()
+# p._eating()
+
+# 4. Create a class Device that turns on, inherit it into Mobile that can make calls, then inherit it into Smartphone that can run apps.
+# class Device:
+#     def turon(self):
+#         print("Turning on Device")
+# class Mobile(Device):
+#     def calls(self):
+#         print("Mobile can make call")
+# class Smartphone(Mobile):
+#     def apps(self):
+#         print("It can run apps")
+# s = Smartphone()
+# s.apps()
+# s.calls()
+# s.turon()
+
+# 5. Create a class Account with balance, inherit it into SavingsAccount that adds interest, then inherit it into PremiumAccount that adds bonus features.
+# class Account:
+#     def __init__(self,balance):
+#         self.balance = balance
+#     def showbal(self):
+#         print("Balance :-",self.balance)
+# class SavingsAccount(Account):
+#     def __init__(self, balance,interest):
+#         super().__init__(balance)
+#         self.interest = interest
+#     def addInterest(self):
+#         self.balance += self.balance*self.interest
+#         print("Balance after Interest :-",self.balance)
+# class PremiumAccount(SavingsAccount):
+#     def __init__(self, balance, interest, bonus):
+#         super().__init__(balance, interest)
+#         self.bonus = bonus
+#     def addBonus(self):
+#         self.balance += self.bonus
+#         print("Balance after Bonus :-",self.balance)
+# pa = PremiumAccount(100,10,20)
+# pa.showbal()
+# pa.addInterest()
+# pa.addBonus()
+
+# 6. Create a class Shape, inherit it into Rectangle to calculate area, then inherit it into ColoredRectangle that adds color information.
+# class Shape:
+#     def __init__(self,length,breadth):
+#         self.length = length
+#         self.breadth = breadth
+#     def show(self):
+#         print("Length :-",self.length)
+#         print("Breadth :-",self.breadth)
+# class Rectangle(Shape):
+#     def __init__(self, length, breadth):
+#         super().__init__(length, breadth)
+#     def area(self):
+#         self.area = 2*(self.length+self.breadth)
+#         print("Area :-",self.area)
+# class ColoredRectangle(Rectangle):
+#     def __init__(self, length, breadth,):
+#         super().__init__(length, breadth)
+#     def colour(self):
+#         self.colour = "Yellow"
+#         print("Colour :-",self.colour)
+# pa = ColoredRectangle(100,10)
+# pa.show()
+# pa.area()
+# pa.colour()
