c650a25f9f4a3f6c45fc10e4d8df9081d538d598
Amisha-Mehta <145385847+Amisha-Mehta@users.noreply.github.com>
2026-02-09 16:15:38 +0530

    Added till js selectors

--- 9-2-26.js
+++ 9-2-26.js
@@ -96,5 +96,70 @@ Current version--> EM(ECMAScript)2025 --> Major update in ES6(2015)
 // }
 
 // Hoisting--> if you call a function before declaring--> var,function--> u can use function or variable with var before the declaration
-console.log(a);
-var a=20;
+// possible with var and normal function only
+// console.log(a);
+// var a=20;
+
+// Arrow function-->
+
+// test=() => {
+//     console.log("Functions........")
+// }
+// test()
+
+// / to take the input in jswrite here and run the html file in pop up the value will be shown
+// input = prompt("Enter value:");
+// alert(input);
+
+
+// const userName=() =>{
+//     // var a;
+//     a=prompt("Enter ur name: ");
+//     return a;
+// }
+// var res=userName();
+// alert(res)
+
+
+// Conditional statements syntax
+
+// for loop
+// for(let i=0;i<=5;i++){
+//     console.log(i);
+// }
+
+// for(var i=0;i<=5;i++){
+//     console.log(i);
+// }
+
+//ternary operator
+// var num=3;
+// let res=num%2==0?"Even":"Odd";
+// console.log(res)
+
+
+//? js selectors--> used to select HTML elements from the DOM
+// getElementByID()
+// getElementsByClassName()
+// getElementsByTagName()
+// querySelector
+// querySelectorAll()
+
+
+//we have change the dom tree with the help of java script
+// getElementById-->use to select only one Element
+// let headingOne=document.getElementById("head");
+// headingOne.style.color="red";
+
+// getElementsByClassName()
+//class return array[through indexing],node list
+// to chnage the color of single single line
+// document.getElementsByClassName("headClass")[2].style.color="red";
+
+let a=document.getElementsByClassName("headClass");
+// a[0].style.color="red";
+// console.log(a.length)
+
+for(var i=0;i<a.length;i++){
+    a[i].style.color="red";
+}
