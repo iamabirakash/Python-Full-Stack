85fc3b2de2a3d834c22b303bda3922bd0c9b06e8
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-02-11 15:57:06 +0530

    Added till saving the notes with models

--- Django/Intro/app/views.py
+++ Django/Intro/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import messages
+from .models import *
 
 def LPU(request):
     return render(request, 'index.html')
@@ -9,14 +10,29 @@ def about(request):
     return render(request, 'about.html')
     # return HttpResponse("wowowowow")
 
-def save_data(req):
-    print(req.POST)
-    title = req.POST.get("title", "")
-    description = req.POST.get("description", "")
-
+def save_data(request):
+    print(request.POST)
+    title = request.POST.get("title", "")
+    description = request.POST.get("description", "")
     if not title or not description:
-            messages.error(req, "Fill all details")
+            messages.error(request, "Fill all details")
             return redirect("/")
+    note = Note(title=title,description=description)
+    note.save()
+    messages.success(request,"Details Saved")
+    return redirect("/")
+    # if request.method == "POST":
+    #     title = request.POST.get("title", "")
+    #     description = request.POST.get("description", "")
 
-        
-    return HttpResponse(f"Details saved")
+    #     if not title or not description:
+    #         messages.error(request, "Fill all details")
+    #         return redirect("/")
+
+    #     Note.objects.create(
+    #         title=title,
+    #         description=description
+    #     )
+
+    #     messages.success(request, "Note added successfully")
+    #     return redirect("/")
