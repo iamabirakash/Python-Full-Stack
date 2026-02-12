9e3658bcd66655d076070e0284c06b42097007ed
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-02-12 09:39:57 +0530

    Added Lambda Functions

---
+++ BASICS/Lambda.py
@@ -0,0 +1,11 @@
+fun = lambda : print("LPU")
+fun()
+
+fun = lambda x : print(x)
+fun("Punjab")
+
+fun = lambda x,y : print("Sum :-",x+y)
+fun(2,5)
+
+fun = lambda x,y : print("Conditional Statement :-",x if x > y else y)
+fun(2,5)
