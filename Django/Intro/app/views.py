cec439118f98c2c8eb24e624d7867b5fb33a08b3
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2026-02-17 13:16:56 +0530

    Added till Working Edit & View

--- Django/Intro/app/views.py
+++ Django/Intro/app/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
 from .models import *
 
 def LPU(request):
-    return render(request, 'index.html')
+    notes = Note.objects.all()
+    return render(request, 'index.html',{'notes':notes})
 
 def about(request):
     return render(request, 'about.html')
     # return HttpResponse("wowowowow")
 
 def save_data(request):
-    print(request.POST)
+    # print(request.POST)
     title = request.POST.get("title", "")
     description = request.POST.get("description", "")
     if not title or not description:
@@ -21,18 +22,33 @@ def save_data(request):
     note.save()
     messages.success(request,"Details Saved")
     return redirect("/")
-    # if request.method == "POST":
-    #     title = request.POST.get("title", "")
-    #     description = request.POST.get("description", "")
 
-    #     if not title or not description:
-    #         messages.error(request, "Fill all details")
-    #         return redirect("/")
+def indexView(request):
+    notes = Note.objects.all()
+    return render(request,"index.html",{'notes':notes})
 
-    #     Note.objects.create(
-    #         title=title,
-    #         description=description
-    #     )
+def deleteView(request, id):
+    note = Note.objects.get(id=id)
+    note.delete()
+    messages.success(request, "Note deleted")
+    return redirect("/")
 
-    #     messages.success(request, "Note added successfully")
-    #     return redirect("/")
+def editView(request, id):
+    note = get_object_or_404(Note, id=id)
+
+    if request.method == "POST":
+        title = request.POST.get("title", "")
+        description = request.POST.get("description", "")
+
+        if not title or not description:
+            messages.error(request, "Fill all details")
+            return redirect(f"/edit/{id}/")
+
+        note.title = title
+        note.description = description
+        note.save()
+
+        messages.success(request, "Note updated successfully")
+        return redirect("/")
+
+    return render(request, "edit.html", {"note": note})
